8cf040e068772a80fa36056aa4f9efb2642ac4bc
Kritchapon Choktananan <iwestz120@gmail.com>
2017-09-25 00:33:39 +0700

    Update Cart and Css

---
+++ page-checkout.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+<?php if( have_posts() ) while( have_posts() ) : the_post(); ?>
+<main class="site_content">
+	<section class="seciton_page seciton_page--checkout">
+		<div class="container container-fluid">
+			<div class="checkout_page__wrapper">
+				<h2 class="checkout_page__title"><?php the_title(); ?></h2>
+				<div class="checkout_page__content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>		
+	</section>
+</main>
+<?php endwhile; ?>
+<?php get_footer(); ?>
